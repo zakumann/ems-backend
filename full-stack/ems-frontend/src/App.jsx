551bc00d783ae14ad8266671a38f76740b431b63
zakumann <samuel742able@gmail.com>
2024-01-16 14:50:56 +0900

    add header and footer

--- full-stack/ems-frontend/src/App.jsx
+++ full-stack/ems-frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
 
   return (
     <>
-      <ListEmployeeComponent />
-
+    <HeaderComponent />
+    <ListEmployeeComponent />
+    <FooterComponent />
     </>
   )
 }
