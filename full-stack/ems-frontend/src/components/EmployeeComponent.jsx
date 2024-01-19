86dd7afd497c2202c5811fc598cffb68e6e423e8
zakumann <samuel742able@gmail.com>
2024-01-19 17:50:13 +0900

    Add Delete function

--- full-stack/ems-frontend/src/components/EmployeeComponent.jsx
+++ full-stack/ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { createEmployee } from '../services/EmployeeService'
+import React, { useEffect, useState } from 'react'
+import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeService'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
@@ -17,17 +17,43 @@ const EmployeeComponent = () => {
 
     const navigator = useNavigate();
 
-    function saveEmployee (e){
+    useEffect(() => {
+
+        if(id){
+            getEmployee(id).then((response) => {
+                setFirstName(response.data.firstName);
+                setLastName(response.data.lastName);
+                setEmail(response.data.email);
+            }).catch(error => {
+                console.error(error);
+            })
+        }
+
+    }, [id]) 
+
+    function saveOrUpdateEmployee(e){
         e.preventDefault();
 
         if(validateForm()){
+
             const employee = {firstName, lastName, email}
             console.log(employee)
 
-            createEmployee(employee).then((response) => {
-                console.log(response.data);
-                navigator('/employees')
-            })
+            if(id){
+                updateEmployee(id, employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees');
+                }).catch(error => {
+                    console.error(error);
+                })
+            } else {
+                createEmployee(employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.error(error);
+                })
+            }
         }
     }
 
@@ -120,7 +146,7 @@ const EmployeeComponent = () => {
                             { errors.email && <div className="invalid-feedback">{ errors.email}</div>}
                         </div>
 
-                        <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                        <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Submit</button>
                     </form>
                 </div>
             </div>
